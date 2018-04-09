3d194b0efdd2b36768c0f496998de00036154ea9
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-04-08 21:58:48 -0700

    Update mothers-love.ino

--- mothers-love.ino
+++ mothers-love.ino
@@ -1,7 +1,7 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -9,16 +9,16 @@ void loop() {
 
 
   
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);
 
 
 
- digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+ digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);
 
 
