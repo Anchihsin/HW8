8baf116550f787d1c26c5edd44b2dd523cb954e6
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-04-08 21:54:54 -0700

    Update urgent.ino

--- urgent.ino
+++ urgent.ino
@@ -1,7 +1,8 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -9,20 +10,21 @@ void loop() {
 
 
   
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);
-
-
-
- digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(6, HIGH); 
+  digitalWrite(6, LOW); 
   delay(100);
 
 
 
-  delay(700);
+ digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+
+
+
+  delay(1500);
 
 }
