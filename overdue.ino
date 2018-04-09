c17a425c2da7bbce542ec7ae6cc06d18c93cbb23
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-04-08 21:58:21 -0700

    Update overdue.ino

--- overdue.ino
+++ overdue.ino
@@ -1,7 +1,9 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -9,16 +11,21 @@ void loop() {
 
 
   
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(500);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(400);
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);
 
 
 
- digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(200);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(6, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);
+
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
   delay(100);
 
 
